f6907cd9c042f5a77c0b65d3e890719e8b5cca42
AronJa <nodararonia@gmail.com>
2021-01-30 01:12:29 +0400

    Added Manufacturer Repository

---
+++ Exercise.Domain/Interfaces/IManufacturerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Exercise.Domain.Records;
+
+namespace Exercise.Domain.Interfaces
+{
+    public interface IManufacturerRepository
+    {
+        Task<IEnumerable<Manufacturer>> GetAllManufacturersAsync();
+        Task<Manufacturer> GetManufacturerByIdAsync(int id);
+        Task AddManufacturerAsync(Manufacturer manufacturer);
+        void UpdateManufacturer(Manufacturer manufacturer);
+        void DeleteManufacturer(Manufacturer manufacturer);
+    }
+}
---
+++ Exercise.Infrastructure/Repositories/ManufacturerRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Exercise.Database;
+using Exercise.Domain.Interfaces;
+using Exercise.Domain.Records;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exercise.Infrastructure.Repositories
+{
+    public class ManufacturerRepository : IManufacturerRepository
+    {
+        private readonly DataContext _context;
+
+        public ManufacturerRepository(DataContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IEnumerable<Manufacturer>> GetAllManufacturersAsync()
+        {
+            return await _context.Manufacturers.ToListAsync();
+        }
+
+        public async Task<Manufacturer> GetManufacturerByIdAsync(int id)
+        {
+            return await _context.Manufacturers.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task AddManufacturerAsync(Manufacturer manufacturer)
+        {
+            await _context.Manufacturers.AddAsync(manufacturer);
+        }
+
+        public void UpdateManufacturer(Manufacturer manufacturer)
+        {
+            _context.Entry(manufacturer).State = EntityState.Modified;
+        }
+
+        public void DeleteManufacturer(Manufacturer manufacturer)
+        {
+            _context.Manufacturers.Remove(manufacturer);
+        }
+    }
+}

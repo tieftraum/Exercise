c7c22bc0d448b7540f84c066cdd8f0338c628bf2
tiefTraum <nodararonia@gmail.com>
2021-01-29 15:56:21 +0400

    cleaning up

--- Exercise.API/Startup.cs
+++ Exercise.API/Startup.cs
@@ -20,21 +20,15 @@ namespace Exercise.API
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Exercise.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Exercise.API", Version = "v1"});
             });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -50,10 +44,7 @@ namespace Exercise.API
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
